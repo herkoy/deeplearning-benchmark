ee7fef9a695d3a681cdb036782708929fd1b4385
herkoy <z1151316339@163.com>
2024-01-19 19:10:59 +0800

    Update setup.sh

--- pytorch/setup.sh
+++ pytorch/setup.sh
@@ -10,8 +10,8 @@ docker pull nvcr.io/nvidia/${NAME_NGC}
 echo "---------------------------------------------------"
 echo "Clone Repos: "
 echo "---------------------------------------------------"
-git clone https://github.com/LambdaLabsML/DeepLearningExamples.git && \
+git clone https://mirror.ghproxy.com/https://github.com/LambdaLabsML/DeepLearningExamples.git && \
 cd DeepLearningExamples && \
 git checkout lambda/benchmark && \
 cd ..
-git clone https://github.com/lambdal/deeplearning-benchmark.git
+git clone https://mirror.ghproxy.com/https://github.com/lambdal/deeplearning-benchmark.git
